a86f05f9a914ae27e7034767ce82330d0ba9b1ec
roadricefield <65438561+roadricefield@users.noreply.github.com>
2020-06-13 13:38:11 +0900

    Modified

--- ChIP_mapping.sh
+++ ChIP_mapping.sh
@@ -8,10 +8,10 @@
 #3. mapping (bowtie)
 #4. duplicate removal (picard).
 
-index="XXX" #Path to your index
+index="XXX" #Path to your bowtie index
 adapter="XXX/Trimmomatic-0.39/adapters/TruSeq3-PE.fa" #Path to your adaptor used by trimmomatic
 
-dname="H3K27Ac_TNFa_0min" #Your condition name
+dname="XXX" #Your condition name
 
 dout="XXX/${dname}" #Path to your output directory named ${dname}
 nthreads=4 #Number of threads used by trimmomatic and fastqc
@@ -22,12 +22,12 @@ reverse_fq="XXX.fastq.gz"
 
 
 # trimming adapters
-java -jar trimmomatic-0.39.jar PE \
+trimmomatic PE \
 -threads ${nthreads} \
 -phred33 \
 ${forward_fq} ${reverse_fq} \
-${dout}/${dname}_forward_paired.fq.gz ${dout}/${dname}_forward_unpaired.fq \
-${dout}/${dname}_reverse_paired.fq.gz ${dout}/${dname}_reverse_unpaired.fq \
+${dout}/${dname}_forward_paired.fq ${dout}/${dname}_forward_unpaired.fq \
+${dout}/${dname}_reverse_paired.fq ${dout}/${dname}_reverse_unpaired.fq \
 ILLUMINACLIP:${adapter}:2:30:10 LEADING:30 TRAILING:30 SLIDINGWINDOW:4:15 MINLEN:30 
 
 
@@ -38,7 +38,6 @@ fastqc --threads ${nthreads} --nogroup -o ${dout} ${dout}/${dname}_reverse_paire
 
 
 # mapping using Bowtie
-
 bowtie -S --best -p 4 ${index} -1 ${dout}/${dname}_forward_paired.fq -2 ${dout}/${dname}_reverse_paired.fq ${dout}/${dname}.sam
 
 
@@ -50,16 +49,13 @@ samtools sort ${dout}/${dname}.bam -o ${dout}/${dname}_sorted.bam
 samtools view -@ 8 -b -F 4 ${dout}/${dname}_sorted.bam > ${dout}/${dname}_tmp.bam
 
 # dupulicate removal
-java -jar picard.jar MarkDuplicates \
+picard MarkDuplicates \
 I=${dout}/${dname}_tmp.bam \
 O=${dout}/${dname}_rm_dups.bam \ #This is your final processed file
 M=${dout}/${dname}_picard_report.txt \
 REMOVE_DUPLICATES=true
 
-samtools index ${dout}/${dname}_rm_dups.bam 
+samtools index ${dout}/${dname}_rm_dups.bam
 
-
-
-
-
-
+#Delete intermediate files 
+rm -rf ${dout}/${dname}_tmp.bam ${dout}/${dname}.sam ${dout}/${dname}_sorted.bam ${dout}/${dname}_forward_unpaired.fq ${dout}/${dname}_reverse_unpaired.fq
